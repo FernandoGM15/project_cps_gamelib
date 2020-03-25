e156dc1c57132bb0adf674525429cff459cd5851
EmilioBG1997 <emilio.barrera1997@gmail.com>
2020-03-24 19:45:43 -0600

    clase otravez

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
 import requests
 import json
+import abc
 
 class juego(object):
   def __init__(self, name:str, desc:str, rating:str, fecha:str, pictures:list , plataforms: list , devs: list, genres: list,ESRB:str):
     self.name=name
     self.desc=desc
     self.rating=rating
+    self.fecha=fecha
+    self.pictures=pictures
+    self.plataforms=plataforms
+    self.devs=devs
+    self.genres=genres
+    self.ESRB=ESRB
+
+  def __str__(self):
+    r=f"\nName: {self.name}\n"
+    r+=f"Description:{self.desc}"
+    r+=f"Rating:{self.rating}"
+    r+=f"Release date:{self.fecha}"
+    r+=f""
 
 
 def get_juego(game: str):
