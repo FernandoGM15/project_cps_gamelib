094f84c486c6960fd214d71c669c05c523e224a7
Fernando Garcia <luis_morales@uadec.edu.mx>
2020-05-24 14:26:16 -0500

    GTA VAL 4.48 to 4.47

--- test_juego.py
+++ test_juego.py
@@ -98,7 +98,7 @@ class test_juego(unittest.TestCase):
                 'ex': {
                     'name': 'Grand Theft Auto V', 
                     'desc': 'Rockstar Games went bigger, since their previous installment of the series. You get the complicated and realistic world-building from Liberty City of GTA4 in the setting of lively and diverse Los Santos, from an old fan favorite GTA San Andreas. 561 different vehicles (including every transport you can operate) and the amount is rising with every update. \r\nSimultaneous storytelling from three unique perspectives: \r\nFollow Michael, ex-criminal living his life of leisure away from the past, Franklin, a kid that seeks the better future, and Trevor, the exact past Michael is trying to run away from. \r\nGTA Online will provide a lot of additional challenge even for the experienced players, coming fresh from the story mode. Now you will have other players around that can help you just as likely as ruin your mission. Every GTA mechanic up to date can be experienced by players through the unique customizable character, and community content paired with the leveling system tends to keep everyone busy and engaged.', 
-                    'rating': 4.48, 
+                    'rating': 4.47, 
                     'fecha': '2013-09-17', 
                     'picture': 'https://media.rawg.io/media/games/b11/b115b2bc6a5957a917bc7601f4abdda2.jpg', 
                     'platforms': ['PC', 'PlayStation 4', 'PlayStation 3', 'Xbox 360', 'Xbox One'], 
@@ -121,10 +121,6 @@ class test_juego(unittest.TestCase):
             except: #Ya que no se pudo crear el objeto juego
                 actual = build_juego(api,i['in'])
             self.assertEqual(actual,i['ex'])
-
-            
-            
-
 
 if __name__=='__main__':
     unittest.main()
